ea749ac17d10b79c14297f326ab979ff1780c277
Michal Pawlowski <misza222@gmail.com>
2018-05-31 11:06:06 +0200

    Prepared to push to gpuwqa

--- my_answers.py
+++ my_answers.py
@@ -12,7 +12,12 @@ def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
-
+    
+    series_len = len(series)
+    
+    X = [series[start: start + window_size] for start in range(0, series_len - window_size)]
+    y = [series[start + window_size]        for start in range(0, series_len - window_size)]
+        
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,13 +28,31 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    
+    model = Sequential()
+    
+    model.add(Dense(32, input_shape=(window_size, 1)))
+    
+    lstm = LSTM(units=window_size, dropout=0.3, recurrent_dropout=0.1)
+    model.add(lstm)
+    
+    output_layer = Dense(1)
+    model.add(output_layer)
+    
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+    
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
-    punctuation = ['!', ',', '.', ':', ';', '?']
-
+    punctuation = ['!', ',', '.', ':', ';', '?', ' ']
+    
+    def is_valid(i):
+        return (i in punctuation) or (ord(i) >= 48 and ord(i) <= 57) or (ord(i) >= 97 and ord(i) <= 122)
+    
+    text = ''.join([i if is_valid(i) else '' for i in text])
+    
     return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -37,10 +60,23 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
+    
+    text_len = len(text)
+    
+    inputs = [text[start:start+window_size] for start in range(0, text_len - window_size, step_size)]
+    outputs= [text[start+window_size]       for start in range(0, text_len - window_size, step_size)]
 
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+
+    lstm = LSTM(units=200, dropout=0.2, recurrent_dropout=0.1, input_shape=(window_size, num_chars))
+    model.add(lstm)
+    
+    output_layer = Dense(num_chars, activation='softmax')
+    model.add(output_layer)
+    
+    return model
